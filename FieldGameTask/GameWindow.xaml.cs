37de6d05f1ab4214fefbe7d9ab2c4c0bf19baeef
Volodymyr Shonbin <shonbin.volodymyr@gmail.com>
2019-04-16 17:01:40 +0300

    Initial commit

---
+++ FieldGameTask/GameWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FieldGameTask
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        double step = 0;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        public GameWindow(double size) : this()
+        {
+            step = (canvasField.Width - 1) / size;
+            DrawField(step);
+            rectangleFigure.Width = step;
+            rectangleFigure.Height = step;
+        }
+
+        private void DrawField(double step)
+        {
+            for (double i = 0; i < canvasField.Width; i += step)
+            {
+                DrawLine(canvasField, 0, i, canvasField.Height, i);
+                DrawLine(canvasField, i, 0, i, canvasField.Width);
+            }
+
+        }
+
+        private void DrawLine(Canvas canvas, double x1, double y1, double x2, double y2)
+        {
+            Line line = new Line
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 1,
+                X1 = x1,
+                X2 = x2,
+                Y1 = y1,
+                Y2 = y2
+            };
+            canvas.Children.Add(line);
+        }
+
+        private void Canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Move Down
+            if (e.Key == Key.S && Canvas.GetTop(rectangleFigure) + rectangleFigure.Height < canvasField.Height - 1.1)
+            {
+                Canvas.SetTop(rectangleFigure, Canvas.GetTop(rectangleFigure) + step);
+            }
+            //Move Up
+            else if (e.Key == Key.W && Canvas.GetTop(rectangleFigure) > 0)
+            {
+                Canvas.SetTop(rectangleFigure, Canvas.GetTop(rectangleFigure) - step);
+            }
+            //Move Left
+            else if (e.Key == Key.A && Canvas.GetLeft(rectangleFigure) > 0)
+            {
+                Canvas.SetLeft(rectangleFigure, Canvas.GetLeft(rectangleFigure) - step);
+
+            }
+            //Move Right
+            else if (e.Key == Key.D && Canvas.GetLeft(rectangleFigure) + rectangleFigure.Width < canvasField.Width - 1.1)
+            {
+                Canvas.SetLeft(rectangleFigure, Canvas.GetLeft(rectangleFigure) + step);
+            }
+        }
+    }
+}
---
+++ FieldGameTask/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FieldGameTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private uint fieldSize = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void ButtonRun_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                fieldSize = uint.Parse(textboxSize.Text);
+                GameWindow game = new GameWindow(fieldSize);
+                game.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // check if not number => not accept
+            if (!IsNumber(e.Text))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private bool IsNumber(string inputText)
+        {
+            return int.TryParse(inputText, out var output);
+        }
+    }
+}
